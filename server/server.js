78384d2b69aa137803da5bf3fae7e07bc5c6489e
ybigus <bigusyaroslav@gmail.com>
2015-01-03 19:23:17 +0200

    add rooms

--- server/server.js
+++ server/server.js
@@ -2,16 +2,24 @@ var io = require('socket.io')(3231);
 var user_count = 0;
 io.on('connection', function(socket){
     //console.log('user connected');
-    user_count++;
-    if(user_count > 2) {
+    var room_id = socket.handshake.query.id;
+    if(io.sockets.adapter.rooms && io.sockets.adapter.rooms[room_id]){
+        var room = io.sockets.adapter.rooms[room_id];
+        user_count = Object.keys(room).length;
+    }
+    else{
+        user_count = 0;
+    }
+    if(user_count >= 2) {
         socket.broadcast.emit('init', {result: false});
         return;
     }
-    socket.emit('init',{result: true, user_side: user_count == 1 ? 'w' : 'b'});
-    io.emit('start',{result: true});
+    socket.join(room_id);
+    socket.emit('init',{result: true, user_side: user_count == 0 ? 'w' : 'b'});
+    if(user_count == 1){
+        io.in(room_id).emit('start',{result: true});
+    }
     socket.on('move', function(msg){
-        //to(room_id)
-        socket.broadcast.emit('move', msg);
-        console.log('test: ' + msg.newX + ':' + msg.newY);
+        socket.broadcast.to(room_id).emit('move', msg);
     });
 });
